f54dc0f5cf4c35812ad7d00e6211d3413c7f0741
David Millan <davidhastt@gmail.com>
2023-09-23 12:07:28 -0600

    Apunte el mapa a Mexico y el zoom lo corregi

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
  
     
     // Inicializar el mapa
-    var map = L.map('map').setView([51.505, -0.09], 13); // Cambia las coordenadas y el nivel de zoom según tu ubicación deseada
+    var map = L.map('map').setView([19.28676640168722, -99.67715832406732], 4); // Cambia las coordenadas y el nivel de zoom según tu ubicación deseada
 
     // Agregar una capa de mapa base (por ejemplo, OpenStreetMap)
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
