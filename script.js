02dfacdee37c3a493bdd721e72bd15e6f155f799
David Millan <davidhastt@gmail.com>
2023-09-23 12:17:20 -0600

    Ya quedo

--- script.js
+++ script.js
@@ -8,3 +8,29 @@
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
+
+
+            // Cargar datos GeoJSON desde el endpoint
+            fetch('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson')
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(data) {
+                // Crear una capa de GeoJSON y agregarla al mapa
+                L.geoJSON(data, {
+                    pointToLayer: function (feature, latlng) {
+                        return L.circleMarker(latlng, {
+                            radius: 5, // Tamaño del marcador
+                            fillColor: 'red', // Color de relleno
+                            color: 'black', // Color del borde
+                            weight: 1, // Grosor del borde
+                            opacity: 1, // Opacidad
+                            fillOpacity: 0.8 // Opacidad del relleno
+                        });
+                    },
+                    onEachFeature: function (feature, layer) {
+                        // Puedes personalizar la información emergente aquí
+                        layer.bindPopup('Magnitud: ' + feature.properties.mag + '<br>Ubicación: ' + feature.properties.place);
+                    }
+                }).addTo(map);
+            });
